61aaa35248b49a1fd93777f8e5196b20d5dce14d
yamasy1549 <sanae@yamasy.info>
2016-10-24 23:35:40 +0900

    HITされたときの処理

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+const axios      = require('axios');
 const express    = require('express');
 const bodyParser = require('body-parser');
 const SerialPort = require("serialport");
 
 const app = express();
+
+var playerId = 0
 
 app.use(bodyParser.urlencoded({
     extended: true
@@ -16,17 +19,40 @@ var port = new SerialPort("/dev/tty.usbmodem1411", {
     baudrate: 9600,
     dataBits: 8,
     parity: 'none',
-    flowControl: false
+    flowControl: false,
+    parser: SerialPort.parsers.readline("\n")
+});
+
+port.on('error', function(err, req, res) {
+    res.end();
 });
 
 port.on('open', function(err) {
     console.log('serial open');
 });
 
+port.on('data', function(data) {
+  console.log('data received: ' + data + ', playerId: ' + playerId); // HITされた
+  var options = {
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    }
+  }
+  axios.put('http://localhost:3000/api/players/' + playerId, { hit: 1 }, options)
+    .then(function(res) {
+        console.log('hit sended');
+    })
+    .catch(function(err) {
+      console.log(err);
+    })
+});
+
 app.post('/api/timer', function(req, res) {
     if(req.body.start) {
         sendSignal('1');
         console.log('start');
+        playerId = req.body.player_id;
         res.send('start');
     } else if(req.body.stop) {
         sendSignal('0');
