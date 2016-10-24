593763c55472a00cecde5d24f8eb6fdc70a82a4e
yamasy1549 <sanae@yamasy.info>
2016-10-24 22:27:41 +0900

    シリアル通信でArduinoに信号を送る

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const express = require('express');
+const express    = require('express');
 const bodyParser = require('body-parser');
+const SerialPort = require("serialport");
 
 const app = express();
 
@@ -8,17 +9,36 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 4444);
 console.log('Server is online.');
+
+var port = new SerialPort("/dev/tty.usbmodem1411", {
+    baudrate: 9600,
+    dataBits: 8,
+    parity: 'none',
+    flowControl: false
+});
+
+port.on('open', function(err) {
+    console.log('serial open');
+});
 
 app.post('/api/timer', function(req, res) {
     if(req.body.start) {
+        sendSignal('1');
         console.log('start');
         res.send('start');
-        // TODO: ここでなんか処理する
     } else if(req.body.stop) {
+        sendSignal('0');
         console.log('stop');
         res.send('stop');
-        // TODO: ここでなんか処理する
     }
-})
+});
+
+function sendSignal(msg) {
+    setTimeout(function() {
+        port.write(msg);
+        console.log('Sended ', msg);
+    }, 200);
+    // NOP的な待ち時間が必要ぽい
+}
